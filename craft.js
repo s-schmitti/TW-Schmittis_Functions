65fb9e50be8a539db2eef0d97815fa7c32b21f9f
s-schmitti <117009161+s-schmitti@users.noreply.github.com>
2022-10-30 12:51:43 +0100

    Create craft.js

---
+++ craft.js
@@ -0,0 +1,70 @@
+(async function(){
+    console.log("extended Crafting")
+    TW_SF.extendFunction(CharacterWindow.Crafting, "init", function(){
+        for(let i = 0 ; i < document.getElementsByClassName("displayValue unselectable").length; i++){
+            document.getElementsByClassName("displayValue unselectable")[i].onclick = function() {
+                const amount = prompt(
+                    (TW_SF.getLocalString("amount") + ': '),
+                );
+                console.log(amount)
+                try {
+                    this.parentElement.getElementsByClassName("butPlus")[0].remove()
+                    this.parentElement.getElementsByClassName("butMinus")[0].remove()
+                } catch(err) {}
+
+                if( amount > 0 )
+                    this.innerText = "" + amount + ""
+            }
+            document.getElementsByClassName("displayValue unselectable")[i].style += ";cursor:pointer"
+        }
+    });
+})()
+
+function ReceipeWatchDog(id){
+    this.id = id;
+    this.canCraft = function(){
+        try {
+            if(this.areEnoughItemsInInventory())
+                return (Crafting.recipes[this.id].last_craft == null) || (Crafting.recipes[this.id].last_craft >= Crafting.recipes[this.id].blocktime);
+        } catch (err) {}
+        return false;
+    }
+
+    this.areEnoughItemsInInventory = function(){
+        let result = true;
+        Crafting.recipes[this.id].resources.forEach(function(ingredient){
+            if(Bag.getItemCount(ingredient.item) < ingredient.count)
+                result = false;
+        })
+        return result;
+    }
+
+    this.start = async function() {
+        await TW_SF.sleep(1 + Math.random()*60);
+        while(true){
+            await TSK.sleep(5 + Math.random()*30);
+            if(this.canCraft()){
+                TW_SF.showMessage(TW_SF.getLocalString("Crafting"), 3600)
+                return;
+            }
+        }
+    }
+    this.start();
+}
+
+(async function() {
+    CharacterWindow.open('crafting');
+    CharacterWindow.window.destroy()
+    if(Character.professionId == 1) { 
+        new ReceipeWatchDog(52524000);
+    }
+    if(Character.professionId == 2){ 
+        new ReceipeWatchDog(52523000);
+    }
+    if(Character.professionId == 3){
+        new ReceipeWatchDog(52526000);
+    }
+    if(Character.professionId == 4){
+        new ReceipeWatchDog(52525000);
+    }
+})();
